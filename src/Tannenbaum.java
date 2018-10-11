3800569ee124505d5e1bb4f9214befbda01d6728
christian <ankert.c@gmail.com>
2018-10-11 21:15:03 +0200

    Finished Kata

--- src/Tannenbaum.java
+++ src/Tannenbaum.java
@@ -1,26 +1,42 @@
 public class Tannenbaum {
-    public int width;
+    public int width = 11;
     public int size;
     private int kugeln = 1;
     int leerstellen;
 
 
     public void zeichnen(){
+
+        // Calculate the first Spacers before the first Star
         leerstellen = (width-kugeln)/2;
 
+        // Print out the rows of the xmastree
         for (int i = 0; i < size; i++){
 
             addLeerstellen();
             addStars();
             addLeerstellen();
+
+           // After each row, remove one spacer, add 2 kugels
             leerstellen = leerstellen - 1;
             kugeln = kugeln + 2;
             System.out.println();
         }
-        leerstellen = (1 + size*2)/2;
+
+        //Calculate the middle of the last row
+        leerstellen = (width)/2;
+
+        // Paint the last row with the tree stump
         addLeerstellen();
         System.out.print("I");
         addLeerstellen();
+
+        // Make Space for the next tree
+        System.out.println();
+        System.out.println();
+
+        //Reset Kugel Value for first Line
+        kugeln = 1;
     }
 
     void addLeerstellen (){
--- src/TannenbaumTestDrive.java
+++ src/TannenbaumTestDrive.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class TannenbaumTestDrive {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
-        int userInputWidth = scanner.nextInt();
+       // int userInputWidth = scanner.nextInt();
         int userInputSize = scanner.nextInt();
         scanner.close();
 
         Tannenbaum xmastree = new Tannenbaum();
         xmastree.size = userInputSize;
-        xmastree.width = userInputWidth;
+       // xmastree.width = userInputWidth;
         xmastree.zeichnen();
     }
 }
